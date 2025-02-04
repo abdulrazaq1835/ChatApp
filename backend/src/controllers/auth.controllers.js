f768471058c2ef3fb77a30fb8a0c87548aa80a09
abdulrazaq1835 <abdulrazzak1835@gmail.com>
2025-02-04 16:53:19 +0530

    commit

---
+++ backend/src/controllers/auth.controllers.js
@@ -0,0 +1,17 @@
+export const signup  = (req, resp)=>{
+    resp.send("signup route")
+
+}
+
+export const login  = (req, resp)=>{
+
+    resp.send("login route")
+
+}
+
+export const logout  = (req, resp)=>{
+    resp.send("logout route") 
+}
+
+
+
